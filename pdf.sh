099239a80c20acb2a6619418e68dc2d86f1bff7c
Rohan Kaushik <100234814+snowkluster@users.noreply.github.com>
2024-04-12 01:14:23 +0530

    Update pdf.sh

--- pdf.sh
+++ pdf.sh
@@ -2,17 +2,28 @@
 
 # the first argument is the original file name
 # the second argument is the output file name 
-# simple script to reduce the size of PDF files
 
 Green="\e[32m"
 Red="\033[0;31m"
 NC='\033[0m'
 
-if [ -x "$(command -v gs)" ]; then
+reduce() {
   printf "%b reducing PDF size %b\n" "$Green" "$NC"
   gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
     -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$2" "$1"
+}
+
+if [ $# -ne 2 ]; then
+  echo "${Red}2 argument must be supplied"
+  echo "first argument is the original PDF file name"
+  printf "second argument is the output PDF file name %b\n" "$NC"
+  exit 1
+fi
+
+if [ -x "$(command -v gs)" ]; then
+ reduce "$1" "$2" 
 else 
   echo "${Red}ghost script not installed ${NC}"
   printf "%b install it using $(sudo apt-get update && sudo apt-get install -y ghostscript) %b\n" "$Green" "$NC"
+  exit 1
 fi
